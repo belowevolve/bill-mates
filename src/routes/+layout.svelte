d9be93547346f956f7ff5f87b5963ac6daaa77bc
belowevolve <130287859+belowevolve@users.noreply.github.com>
2025-01-12 03:40:26 +0300

    fix: https on cache

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,6 @@
   import { onMount } from 'svelte'
 
   async function detectSWUpdate() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then(() => {
-          console.log('ServiceWorker registration successful')
-        })
-        .catch((err) => {
-          console.log('ServiceWorker registration failed: ', err)
-        })
-    }
     const registration = await navigator.serviceWorker.ready
     registration.addEventListener('updatefound', () => {
       const newSW = registration.installing
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,11 +4,16 @@ import { build, files, version } from '$service-worker'
 
 declare let self: ServiceWorkerGlobalScope
 
+// Create a unique cache name for this deployment
 const CACHE = `cache-${version}`
-const ASSETS = [...build, ...files]
 
-// install service worker
+const ASSETS = [
+  ...build, // the app itself
+  ...files // everything in `static`
+]
+
 self.addEventListener('install', (event) => {
+  // Create a new cache and add all files to it
   async function addFilesToCache() {
     const cache = await caches.open(CACHE)
     await cache.addAll(ASSETS)
@@ -17,8 +22,8 @@ self.addEventListener('install', (event) => {
   event.waitUntil(addFilesToCache())
 })
 
-// activate service worker
 self.addEventListener('activate', (event) => {
+  // Remove previous cached data from disk
   async function deleteOldCaches() {
     for (const key of await caches.keys()) {
       if (key !== CACHE) await caches.delete(key)
@@ -28,39 +33,54 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(deleteOldCaches())
 })
 
-// listen to fetch events
 self.addEventListener('fetch', (event) => {
+  // ignore POST requests etc
   if (event.request.method !== 'GET') return
 
   async function respond() {
     const url = new URL(event.request.url)
     const cache = await caches.open(CACHE)
-    // serve build files from the cache
+
+    // `build`/`files` can always be served from the cache
     if (ASSETS.includes(url.pathname)) {
-      const cachedResponse = await cache.match(url.pathname)
-      if (cachedResponse) return cachedResponse
+      const response = await cache.match(url.pathname)
+
+      if (response) {
+        return response
+      }
     }
 
-    // try network first
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
     try {
       const response = await fetch(event.request)
+
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
       if (!(response instanceof Response)) {
         throw new Error('invalid response from fetch')
       }
-      const isNotExtension = url.protocol === 'http:'
-      const isSuccess = response.status === 200
-      if (isNotExtension && isSuccess) cache.put(event.request, response.clone())
+      const isNotExtension = url.protocol === 'http:' || url.protocol === 'https:'
+      if (response.status === 200 && isNotExtension) {
+        cache.put(event.request, response.clone())
+      }
+
       return response
     } catch (err) {
-      // fallback to cache
-      const cachedResponse = await cache.match(event.request)
-      if (cachedResponse) return cachedResponse
+      const response = await cache.match(event.request)
+
+      if (response) {
+        return response
+      }
+
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
       throw err
     }
   }
+
   event.respondWith(respond())
 })
-
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
 })
